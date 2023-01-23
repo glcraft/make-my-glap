0e047211adafd05ed8b5af1e42422aa1d0adcfbd
Gly <gabin.lefranc@gmail.com>
2023-01-23 23:54:14 +0100

    add includes in yaml

--- src/generators/header.cpp
+++ src/generators/header.cpp
@@ -12,11 +12,21 @@
 auto header_program(const YAML::Node& config, std::ofstream& output) -> Result<int>;
 
 auto generate_header(YAML::Node config, std::ofstream output) -> Result<int> {
+    auto program_config = config["program"];
+    if (!program_config.IsDefined() || program_config.IsNull()) {
+        return tl::make_unexpected(std::string{"No program defined"});
+    }
     output << "#pragma once\n\n";
 #ifdef GLAP_USE_MODULE
     output << "import glap;\n\n";
 #else 
-    output << "#include <glap/glap.h>\n\n";
+    output << "#include <glap/glap.h>\n";
 #endif
-    return header_program(config["program"], output);
+    if (auto includes = program_config["includes"]; includes.IsDefined() && !includes.IsNull() && includes.IsSequence()) {
+        for (auto include : includes) {
+            output << "#include " << include.as<std::string>() << '\n';
+        }
+    }
+    output << '\n';
+    return header_program(program_config, output);
 }
