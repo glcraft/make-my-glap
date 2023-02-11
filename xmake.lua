64f595e12be4e14c69863492b949e2276b3066ee
Gly <gabin.lefranc@gmail.com>
2023-02-11 21:46:50 +0100

    glap is deps if is in constellation

--- xmake.lua
+++ xmake.lua
@@ -3,7 +3,10 @@ includes("xmake/**/xmake.lua")
 add_requires("yaml-cpp")
 add_requires("tl_expected")
 add_requires("fmt")
-add_requires("glap")
+
+if not constellation then
+    add_requires("glap")
+end
 
 target("mmg")
     set_kind("binary")
@@ -12,6 +15,11 @@ target("mmg")
     add_files("src/**.cpp")
     add_includedirs("include")
     add_packages("yaml-cpp", "fmt", "tl_expected", "glap")
+    if constellation then
+        add_deps("glap")
+    else 
+        add_packages("glap")
+    end
     -- add_rules("glap.mmg")
     -- add_files("mmg/args.glap.yml")
     add_installfiles("xmake/rules/(**/*.lua)", {prefixdir = "rules"})
